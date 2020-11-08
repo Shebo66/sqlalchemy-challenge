063ff98cf913dffce12a010bd4cc9292e039851b
Shebo66 <70512412+Shebo66@users.noreply.github.com>
2020-11-09 00:46:45 +0800

    homework84

--- app.py
+++ app.py
@@ -12,6 +12,7 @@ from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
 
 from flask import Flask, jsonify
+import datetime as dt
 
 
 #################################################
@@ -84,6 +85,41 @@ def prcp():
 
     return jsonify(all_prcp)
 
+
+@app.route("/api/v1.0/tobs")
+def tobs():
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+
+    """Return a list of all tobs"""
+    # Query all stations
+    last_year=dt.date(2017,8,23)-dt.timedelta(days=365)
+    temperature = session.query(measurement.tobs).filter(measurement.station=='USC00519281').filter(measurement.date>=last_year).all()
+    session.close()
+
+    # Convert list of tuples into normal list
+    #all_prcp = list(np.ravel(results))
+    all_tobs = temperature
+
+    return jsonify(all_tobs)
+
+
+@app.route("/api/v1.0/temp/<start><end>")
+def temp(start,end):
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+
+    """Return a list of all tobs"""
+    # Query all stations
+    temp = session.query(func.avg(measurement.tobs), func.max(measurement.tobs), func.min(measurement.tobs)).filter(measurement.date>=start).filter(measurement.date<=end).all()
+     
+    session.close()
+
+    # Convert list of tuples into normal list
+    #all_prcp = list(np.ravel(results))
+    all_temp = temp
+
+    return jsonify(all_temp)
 
 if __name__ == '__main__':
     app.run(debug=True)
