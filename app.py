fc14d5b8b41f50b21ebcc8203b047e5fb5ce6f65
Shebo66 <70512412+Shebo66@users.noreply.github.com>
2020-11-09 00:14:04 +0800

    homework83

---
+++ app.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import numpy as np
+
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+from flask import Flask, jsonify
+
+
+#################################################
+# Database Setup
+#################################################
+engine = create_engine("sqlite:///hawaii.sqlite")
+
+# reflect an existing database into a new model
+Base = automap_base()
+# reflect the tables
+Base.prepare(engine, reflect=True)
+
+# Save reference to the table
+measurement = Base.classes.measurement
+station = Base.classes.station
+
+#################################################
+# Flask Setup
+#################################################
+app = Flask(__name__)
+
+
+#################################################
+# Flask Routes
+#################################################
+
+@app.route("/")
+def welcome():
+    """List all available api routes."""
+    return (
+        f"Available Routes:<br/>"
+        f"/api/v1.0/prcp<br/>"
+        f"/api/v1.0/temp<br/>"
+        f"/api/v1.0/tobs<br/>"
+        f"/api/v1.0/stations"
+    )
+
+
+@app.route("/api/v1.0/stations")
+def names():
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+
+    """Return a list of all stations names"""
+    # Query all stations
+    results = session.query(station.name).all()
+
+    session.close()
+
+    # Convert list of tuples into normal list
+    all_stations = list(np.ravel(results))
+
+    return jsonify(all_stations)
+
+
+@app.route("/api/v1.0/prcp")
+def prcp():
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+
+    """Return a list of all prcp"""
+    # Query all stations
+    results = session.query(measurement.date, measurement.prcp).all()
+
+    session.close()
+
+    # Convert list of tuples into normal list
+    #all_prcp = list(np.ravel(results))
+    all_prcp = results
+
+    return jsonify(all_prcp)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
